2085365043d971195286b9cec31c4ce5aa2ede57
magnusbarata <barata.magnuz@gmail.com>
2020-10-12 01:29:49 +0900

    solved 4-1

---
+++ chapter4/TreesGraphs.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+class Graph<T> {
+    private ArrayList<GraphNode<T>> vertices = new ArrayList<>();
+
+    public void addVertex(GraphNode<T> v) { vertices.add(v); }
+    public ArrayList<GraphNode<T>> getVertices() { return vertices; }
+
+    /* 4-1 */
+    private void setStateUnvisited() {
+        for (GraphNode<T> n: vertices) n.state = NodeState.unvisited;
+    }
+
+    public boolean DFS(GraphNode<T> start, GraphNode<T> target) {
+        setStateUnvisited();
+        ArrayList<GraphNode<T>> stack = new ArrayList<>(); // Use array list as stack
+        stack.add(start);
+        while (!stack.isEmpty()) {
+            GraphNode<T> current = stack.remove(stack.size() - 1);
+            if (current != null && current.state == NodeState.unvisited) {
+                if (current == target) return true;
+                stack.addAll(current.getAdjacent());
+                current.state = NodeState.visited;
+            }
+        }
+        return false;
+    }
+    
+    public boolean BFS(GraphNode<T> start, GraphNode<T> target) {
+        setStateUnvisited();
+        ArrayList<GraphNode<T>> queue = new ArrayList<>(); // Use array list as queue
+        queue.add(start);
+        while (!queue.isEmpty()) {
+            GraphNode<T> current = queue.remove(0);
+            if (current != null && current.state == NodeState.unvisited) {
+                if (current == target) return true;
+                queue.addAll(current.getAdjacent());
+                current.state = NodeState.visited;
+            }
+        }
+        return false;
+    }
+}
+
+public class TreesGraphs {
+    static final String ANSI_RESET = "\u001B[0m";
+    static final String ANSI_GREEN = "\u001B[32m";
+    
+    @SuppressWarnings("unchecked")
+    private static void check1() {
+        GraphNode<String>[] nodes = new GraphNode[]{
+            new GraphNode<>("a"),
+            new GraphNode<>("b"),
+            new GraphNode<>("c"),
+            new GraphNode<>("d"),
+            new GraphNode<>("e"),
+            new GraphNode<>("f"),
+        };
+        nodes[0].addAdjacent(nodes[1]);
+        nodes[0].addAdjacent(nodes[2]);
+        nodes[0].addAdjacent(nodes[3]);
+        nodes[3].addAdjacent(nodes[4]);
+        nodes[4].addAdjacent(nodes[5]);
+        nodes[5].addAdjacent(nodes[0]);
+
+        Graph<String> g = new Graph<>();
+        for (int i = 0; i < 6; i++) g.addVertex(nodes[i]);
+        assert g.BFS(nodes[3], nodes[5]) : "(BFS) There is a route between node 3 and 5";
+        assert g.BFS(nodes[5], nodes[1]) : "(BFS) There is a route between node 5 and 1";
+        assert !g.BFS(nodes[1], nodes[5]) : "(BFS) There is no route between node 1 and 5";
+        assert g.DFS(nodes[3], nodes[5]) : "(DFS) There is a route between node 3 and 5";
+        assert g.DFS(nodes[5], nodes[1]) : "(DFS) There is a route between node 5 and 1";
+        assert !g.DFS(nodes[1], nodes[5]) : "(DFS) There is no route between node 1 and 5";
+    }
+
+    public static void main(String[] args) {
+        System.out.print("4-1: ");
+        check1();
+        System.out.println(ANSI_GREEN + "OK!" + ANSI_RESET);
+    }
+}
